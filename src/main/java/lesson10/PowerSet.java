69ab9a07811958bec8faa33deb934bf1e4627ab7
sheykin <sheykindms@gmail.com>
2021-06-06 00:42:29 +0300

    Add set impl Add tests

---
+++ src/main/java/lesson10/PowerSet.java
@@ -0,0 +1,77 @@
+package lesson10;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PowerSet {
+
+  private final List<String> set;
+
+  public PowerSet() {
+    set = new ArrayList<>(20000);
+  }
+
+  public int size() {
+    return set.size();
+  }
+
+  public void put(String value) {
+    if (!set.contains(value)) {
+      set.add(value);
+    }
+  }
+
+  public boolean get(String value) {
+    return set.contains(value);
+  }
+
+  public boolean remove(String value) {
+    return set.remove(value);
+  }
+
+  public PowerSet intersection(PowerSet set2) {
+    var powerSet = new PowerSet();
+    for (String s : set) {
+      if (set2.get(s)) {
+        powerSet.put(s);
+      }
+    }
+    return powerSet;
+  }
+
+  public PowerSet union(PowerSet set2) {
+    var powerSet = new PowerSet();
+    for (String s : set) {
+      powerSet.put(s);
+    }
+    for (int i = 0; i < set2.size(); i++) {
+      if (!powerSet.get(set2.getElement(i))) {
+        powerSet.put(set2.getElement(i));
+      }
+    }
+    return powerSet;
+  }
+
+  public PowerSet difference(PowerSet set2) {
+    var powerSet = new PowerSet();
+    for (String s : set) {
+      if (!set2.get(s)) {
+        powerSet.put(s);
+      }
+    }
+    return powerSet;
+  }
+
+  public boolean isSubset(PowerSet set2) {
+    for (int i = 0; i < set2.size(); i++) {
+      if (!get(set2.getElement(i))) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private String getElement(int index) {
+    return set.get(index);
+  }
+}
---
+++ src/test/java/lesson10/PowerSetTest.java
@@ -0,0 +1,86 @@
+package lesson10;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PowerSetTest {
+
+  PowerSet set1;
+  PowerSet set2;
+
+  @BeforeEach
+  void setUp() {
+    set1 = new PowerSet();
+    set2 = new PowerSet();
+  }
+
+  @Test
+  void intersection() {
+    set1.put("test1");
+    set1.put("test2");
+    set1.put("test3");
+
+    set2.put("test3");
+    set2.put("test4");
+    set2.put("test5");
+
+    PowerSet intersection = set1.intersection(set2);
+
+    assertTrue(intersection.get("test3"));
+    assertFalse(intersection.get("test1"));
+    assertFalse(intersection.get("test5"));
+  }
+
+  @Test
+  void union() {
+    set1.put("test1");
+    set1.put("test2");
+    set1.put("test3");
+
+    set2.put("test3");
+    set2.put("test4");
+    set2.put("test5");
+
+    PowerSet union = set1.union(set2);
+    assertEquals(5, union.size());
+    assertTrue(union.get("test3"));
+    assertTrue(union.get("test1"));
+    assertTrue(union.get("test5"));
+  }
+
+  @Test
+  void difference() {
+    set1.put("test1");
+    set1.put("test2");
+    set1.put("test3");
+    set1.put("test4");
+    set1.put("test5");
+
+    set2.put("test3");
+    set2.put("test4");
+    set2.put("test5");
+
+    PowerSet difference = set1.difference(set2);
+    assertTrue(difference.get("test1"));
+    assertTrue(difference.get("test2"));
+    assertFalse(difference.get("test3"));
+    assertFalse(difference.get("test4"));
+    assertFalse(difference.get("test5"));
+  }
+
+  @Test
+  void isSubset() {
+    set1.put("test1");
+    set1.put("test2");
+    set1.put("test3");
+    set1.put("test4");
+    set1.put("test5");
+
+    set2.put("test3");
+    set2.put("test4");
+    set2.put("test5");
+
+    assertTrue(set1.isSubset(set2));
+  }
+}
