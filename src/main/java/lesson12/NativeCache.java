a79990216610c848d79c268403ddeb8154c83ea2
sheykin <sheykindms@gmail.com>
2021-06-07 19:38:04 +0300

    Add simple cache impl Add test

---
+++ src/main/java/lesson12/NativeCache.java
@@ -0,0 +1,91 @@
+package lesson12;
+
+import java.lang.reflect.Array;
+
+public class NativeCache<T> {
+
+  public int size;
+  public String[] slots;
+  public T[] values;
+  public int[] hits;
+
+  public NativeCache(int size, Class clazz) {
+    this.size = size;
+    slots = new String[size];
+    values = (T[]) Array.newInstance(clazz, size);
+    hits = new int[size];
+  }
+
+  public int hashFun(String key) {
+    if (key.isEmpty()) {
+      return 0;
+    }
+    var hash = 1;
+    for (char c : key.toCharArray()) {
+      hash = hash * 31 + c;
+    }
+    return Math.abs(hash) % size;
+  }
+
+  public boolean isKey(String key) {
+    var index = seekSlot(key);
+    if (index == -1) {
+      return false;
+    }
+    return slots[index] != null;
+  }
+
+  public void put(String key, T value) {
+    var index = seekSlot(key);
+    if (index == -1) {
+      replaceWithNew(key, value);
+    } else {
+      slots[index] = key;
+      values[index] = value;
+      hits[index]++;
+    }
+  }
+
+  public T get(String key) {
+    var index = seekSlot(key);
+    if (index == -1 || slots[index] == null) {
+      return null;
+    }
+    hits[index]++;
+    return values[index];
+  }
+
+  private void replaceWithNew(String key, T value) {
+    int min = hits[0];
+    var index = 0;
+    for (var i = 0; i < hits.length; i++) {
+      if (min > hits[i]) {
+        min = hits[i];
+        index = i;
+      }
+    }
+    slots[index] = key;
+    values[index] = value;
+    hits[index] = 1;
+  }
+
+  private int seekSlot(String value) {
+    var index = hashFun(value);
+    var step = 1;
+    var iterations = 0;
+    while (slots[index] != null) {
+      if (slots[index].equals(value)) {
+        return index;
+      }
+      index += step;
+      if (index >= size && iterations < step) {
+        index = iterations;
+        iterations++;
+      }
+      if (index >= size) {
+        return -1;
+      }
+    }
+    return index;
+  }
+}
---
+++ src/test/java/lesson12/NativeCacheTest.java
@@ -0,0 +1,38 @@
+package lesson12;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NativeCacheTest {
+
+  NativeCache<String> cache;
+
+  @BeforeEach
+  void setUp() {
+    cache = new NativeCache<>(3, String.class);
+  }
+
+  @Test
+  void putThreeThenGetTwoThenPutOneThenValidate() {
+    cache.put("test1", "val1");
+    cache.put("test2", "val2");
+    cache.put("test3", "val3");
+
+    assertEquals("val1", cache.get("test1"));
+    assertEquals("val1", cache.get("test1"));
+    assertEquals("val2", cache.get("test2"));
+
+    cache.put("new", "element");
+    assertEquals("element", cache.get("new"));
+    assertNull(cache.get("test3"));
+
+    cache.put("last", "one");
+    assertEquals("one", cache.get("last"));
+    assertNull(cache.get("test2"));
+  }
+}
