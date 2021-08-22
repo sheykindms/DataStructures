dd47f696fdae0ef5b2441b1eccb2ecacc012c3b4
sheykindms <sheykindms@gmail.com>
2021-08-22 22:12:28 +0300

    Add Array implementation of Binary Search Tree

---
+++ src/main/java/lesson15/aBST.java
@@ -0,0 +1,46 @@
+package lesson15;
+
+import java.util.*;
+
+class aBST {
+    public Integer[] Tree;
+    private final int depth;
+
+    public aBST(int depth) {
+        int numberOfNodes = (int) (Math.pow(2, depth + 1) - 1);
+        Tree = new Integer[numberOfNodes];
+        for (int i = 0; i < numberOfNodes; i++) {
+            Tree[i] = null;
+        }
+        this.depth = depth;
+    }
+
+    public Integer FindKeyIndex(int key) {
+        int currentDepth = 0;
+        int index = 0;
+        while (currentDepth <= depth) {
+            if (Tree[index] == null) {
+                return -index;
+            } else if (Tree[index] == key) {
+                return index;
+            }
+            if (key < Tree[index]) {
+                index = index * 2 + 1;
+            } else {
+                index = index * 2 + 2;
+            }
+            currentDepth++;
+        }
+        return null;
+    }
+
+    public int AddKey(int key) {
+        Integer index = FindKeyIndex(key);
+        if (index == null) {
+            return -1;
+        }
+        Tree[-index] = key;
+        return -index;
+    }
+
+}
